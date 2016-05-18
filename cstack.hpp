0f3a5940e8461eb2b71e012cbebdd87f876bd300
maked0n <alex.mikhalevich@gmail.com>
2016-05-18 19:41:41 +0300

    Lock-free stack implementation

---
+++ cstack.hpp
@@ -0,0 +1,59 @@
+#include <atomic>
+#include <memory>
+
+template<typename T>
+class CStack {
+	private:
+		struct node;
+		struct counted_node_ptr {
+			int external_count;
+			node* ptr;
+		};
+		struct node {
+			std::shared_ptr<T> data;
+			std::atomic<int> internal_count;
+			counted_node_ptr next;
+			node(const T& data_) : data(std::make_shared<T>(data_)), internal_count(0) {}
+		};
+		std::atomic<counted_node_ptr> head;
+
+		void IncreaseHeadCount(counted_node_ptr& old_counter) {
+			counted_node_ptr new_counter;
+			do {
+				new_counter = old_counter;
+				++new_counter.external_count;
+			} while(!head.compare_exchange_strong(old_counter, new_counter, std::memory_order_acquire, std::memory_order_relaxed));
+			old_counter.external_count = new_counter.external_count;
+		}
+	public:
+		~CStack() {
+			while(pop());
+		}
+
+		void push(const T& data) {
+			counted_node_ptr new_node;
+			new_node.ptr = new node(data);
+			new_node.external_count = 1;
+			new_node.ptr->next = head.load(std::memory_order_relaxed) 
+				while(!head.compare_exchange_weak(new_node.ptr->next, new_node, std::memory_order_release, std::memory_order_relaxed));
+		}
+		std::shared_ptr<T> pop() {
+			counted_node_ptr old_head = head.load(std::memory_order_relaxed);
+			for(;;) {
+				IncreaseHeadCount(old_head);
+				const node* ptr = old_head.ptr;
+				if(!ptr) return std::shared_ptr<T>();
+				if(head.compare_exchange_strong(old_head, ptr->next, std::memory_order_relaxed)) {
+					std::shared_ptr<T> res;
+					res.swap(ptr->data);
+					const int count_increase = old_head.external_count - 2;
+					if(ptr->internal_count.fetch_add(count_increase, std::memory_order_release) == -count_increase) delete ptr;
+					return res;
+				}
+				else if(ptr->internal_count.fetch_add(-1, std::memory_order_relaxed) == 1) {
+					ptr->internal_count.load(std::memory_order_acquire);
+					delete ptr;
+				}
+			}
+		}
+};
